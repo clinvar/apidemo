29cfdc4740ab17b2700bd2dc7e484b0172a81995
Xin Feng <xinf@bcm.edu>
2015-05-21 11:13:08 -0500

    wow

--- up.rb
+++ up.rb
@@ -1,6 +1,5 @@
 def getUP
- ["xin","123456"] #Provide your credentials here
- # ["xfengutsw","123456"] #Provide your credentials here
+ []#Provide your credentials here
           #EXAMPLE: ["username","password"]
 end
   
